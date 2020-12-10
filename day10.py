b43a70c86a1b29eef0effa9c21771dbe04ca5514
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2020-12-09 22:31:55 -0800

    Iterative solution to day10

--- day10.py
+++ day10.py
@@ -2,6 +2,7 @@
 
 from get_input import get_input, line_parser
 from collections import defaultdict
+import functools
 
 
 def part1(adaptors):
@@ -14,10 +15,26 @@ def part2(adaptors):
     assert len(set(adaptors)) == len(adaptors)
     chain = [0] + sorted(adaptors)
     paths = defaultdict(int, {0: 1})
-    for adaptor in chain:
-        for value in [a for a in chain if 1 <= a - adaptor <= 3]:
+    for i, adaptor in enumerate(chain):
+        for value in chain[i+1:]:
+            if value - adaptor > 3:
+                break
             paths[value] += paths[adaptor]
     return paths[max(adaptors)]
+
+
+def part2_recurse(adaptors):
+    assert len(set(adaptors)) == len(adaptors)
+
+    @functools.lru_cache
+    def paths(step):
+        if step == 0:
+            return 1
+        if step not in adaptors:
+            return 0
+        return paths(step-1) + paths(step-2) + paths(step-3)
+
+    return paths(max(adaptors))
 
 
 TEST1 = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
@@ -27,3 +44,4 @@ if __name__ == "__main__":
     LINES = line_parser(get_input(day=10, year=2020))
     print(f"Part 1: {part1(LINES)}")
     print(f"Part 2: {part2(LINES)}")
+    assert part2_recurse(LINES) == part2(LINES)
