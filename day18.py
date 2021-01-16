1bbb06f8269f312d3f907fb4f9b6c96e9e86125d
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2021-01-15 21:13:51 -0800

    Solution to day18 using real algorythm

--- day18.py
+++ day18.py
@@ -1,107 +1,73 @@
 """Solution to day 18 of Advent of Code"""
 
 from get_input import get_input, line_parser
-from operator import add, mul
 import re
 
 
-def find_match_index(symbols):
-    stack = 0
-    for j, sym in enumerate(symbols):
-        if sym == ')':
-            stack += 1
-        elif sym == '(':
-            stack -= 1
-        if stack == 0:
-            return j
+def rpn_evaluate(tokens):
+    result = []
+    for token in tokens:
+        if isinstance(token, int):
+            result.append(token)
+        elif token == '+':
+            result.append(result.pop() + result.pop())
+        elif token == '*':
+            result.append(result.pop() * result.pop())
+        else:
+            raise NotImplementedError
+    assert len(result) == 1
+    return result[0]
 
 
-# def evaluate(symbols):
-#     if len(symbols) == 1 and isinstance(symbols[0], int):
-#         return symbols[0]
-#     elif symbols[0] == ')':
-#         j = find_match_index(symbols)
-#         lnum = evaluate(symbols[1:j])
-#         if len(symbols) <= j+1:
-#             return lnum
-#         op = symbols[j+1]
-#         rnum = evaluate(symbols[j+2:])
-#     else:
-#         lnum, op = symbols[:2]
-#         rnum = evaluate(symbols[2:])
-#     return eval(f"{int(lnum)}{op}{int(rnum)}")
-
-
-def find_match_index_v2(symbols):
-    stack = 0
-    for j, sym in enumerate(symbols):
-        if sym == '(':
-            stack += 1
-        elif sym == ')':
-            stack -= 1
-        if stack == 0:
-            return j
-
-
-def evaluate_v2(symbols):
-    symbols = list(symbols)
-    if len(symbols) == 1 and isinstance(symbols[0], int):
-        return symbols[0]
-    elif symbols[0] == '(':
-        j = find_match_index(symbols)
-        num = evaluate_v2(symbols[1:j])
-        symbols[0:j+1] = [num]
-    elif symbols[1] == '+':
-        if isinstance(symbols[2], int):
-            symbols[:3] = [symbols[0] + symbols[2]]
-        elif symbols[2] == '(':
-            j = find_match_index_v2(symbols[2:]) + 2
-            symbols[:j+1] = [symbols[0] + evaluate_v2(symbols[3:j])]
-    elif symbols[1] == '*':
-        symbols[0:] = [symbols[0] * evaluate_v2(symbols[2:])]
-    else:
-        raise NotImplementedError()
-    return evaluate_v2(symbols)
-
-
-def evaluate(symbols):
-    operators = []
+def rpn_parse(tokens, prec):
+    stack = []
     output = []
-    for symbol in symbols:
-        if isinstance(symbol, int):
-            output.append(symbol)
-        elif symbol == '+':
-            operators.append(add)
-        elif symbol == '*':
-            operators.append(mul)
-        elif symbol == '(':
-            operators.append('(')
-        elif symbol == ')':
-            symbol = operators.pop()
-            while symbol != '(':
-                output.append(symbol)
-                symbol = operators.pop()
-    breakpoint()
-    while True:
-        operator = operators.pop()
-        opera
+    for token in tokens:
+        if isinstance(token, int):
+            output.append(token)
+        elif token == '(':
+            stack.append(token)
+        elif token == ')':
+            while True:
+                temp = stack.pop()
+                if temp == '(':
+                    break
+                output.append(temp)
+        else:
+            while len(stack) > 0 and prec(stack[-1], token):
+                output.append(stack.pop())
+            stack.append(token)
+    return output + stack[::-1]
 
 
 def part1(lines):
     total = 0
+
+    def comp(a, b):
+        return a in ('*', '+') and b in ('*', '+')
+
     for line in lines:
-        val = evaluate(line)
-        total += val
+        tokens = rpn_parse(line, comp)
+        total += rpn_evaluate(tokens)
     return total
 
 
 def part2(lines):
     total = 0
+
+    def comp(a, b):
+        if a == '+':
+            return a != b
+        elif a == '*':
+            if b == '+':
+                return False
+            return a != b
+        return False
+
     for line in lines:
-        val = evaluate_v2(line)
-        total += val
+        tokens = rpn_parse(line, comp)
+        total += rpn_evaluate(tokens)
     return total
-    raise NotImplementedError
 
 
 def parse(line):
